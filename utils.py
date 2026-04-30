9dfb4deac1b9604cedd723fba14c7c9d863d6032
Maria Alejandra <malejab2409@gmail.com>
2026-04-30 12:33:25 -0500

    p5c

--- utils.py
+++ utils.py
@@ -1,1 +1,20 @@
-# utils.py
+from bank_account import BankAccount
+from person import Person
+def person_data():
+    nombre= input("Enter the person's name:")
+    person = Person(nombre)
+    while True:
+        numero_cuenta = int(input("Enter a 4-digit account number:"))
+        saldo_inicial = float(input("Enter the initial balance:"))
+        cuenta = BankAccount(numero_cuenta, saldo_inicial)
+        person.accounts.append(cuenta)
+        añadir = input("Are you done adding accounts? (yes/no):")
+        if añadir == "yes":
+            break
+        return person 
+    def balance_summary(person_list):
+        for person in person_list:
+            total = 0
+            for account in person.accounts: 
+                total += account.balance
+            print(f"{person.nombre} : {total:.2f}")
