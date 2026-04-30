f592ad1e5baa6f05f52b2ba5d643ab869f739173
Maria Alejandra <malejab2409@gmail.com>
2026-04-30 12:15:45 -0500

    p5b

--- person.py
+++ person.py
@@ -1,1 +1,8 @@
-# person.py
+class Person:
+    def _init_(self, name):
+        self.name = name 
+        self.cuenta = []
+    def add_account(self, account):
+        self.cuenta.append(account)
+    def _str_(self): 
+        return f"Name = {self.name}, Number of accounts = {len(self.cuenta)} "
